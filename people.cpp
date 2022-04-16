536c21c9e3d991717edb5b35d216358311c503c8
luqinqinzhenkeai <2650203054@qq.com>
2022-04-17 01:33:11 +0800

    首次提交people文件

---
+++ people.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<unistd.h>
+#include<unistd.h>
+
+using namespace std;
+
+class Cperson
+{
+
+	private:
+		Cperson(){}
+		~Cperson(){}
+	private:
+		static Cperson * ps;
+		static pthread_mutex_t lock;
+	public:
+		static Cperson * Getobject();
+		static void Destroy(Cperson * p);
+
+}
